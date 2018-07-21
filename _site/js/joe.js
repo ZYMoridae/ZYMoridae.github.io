d9ce4d02b8fb34084ecef726cc8d6db8d57c8572
Joe <ashzhouyue@gmail.com>
2018-07-21 20:48:35 +1000

    [TWEAK] new employment

--- _site/js/joe.js
+++ _site/js/joe.js
@@ -1,11 +1,11 @@
-var color_list = ["#cbdbff","#92c1bf","#dbe5ac","#ede0b2","#edc3b2","#d19daf"]
+// var color_list = ["#cbdbff","#92c1bf","#dbe5ac","#ede0b2","#edc3b2","#d19daf"]
 
 
-$(".sec-books div, .sec-skills div").each(function (index, element) {
-  var index_color = Math.floor((Math.random() * 6)) % 6;
-  $(this).css({'background-color': color_list[index_color]});
-});
+// $(".sec-books div, .sec-skills div").each(function (index, element) {
+//   var index_color = Math.floor((Math.random() * 6)) % 6;
+//   $(this).css({'background-color': color_list[index_color]});
+// });
 
-$(".zy-logo").click(function(){
-  $('.post-listing').animate({scrollTop:0}, 'slow');
-});
+// $(".zy-logo").click(function(){
+//   $('.post-listing').animate({scrollTop:0}, 'slow');
+// });
